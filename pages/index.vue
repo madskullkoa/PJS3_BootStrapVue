b6e83d4251370daecd9da4aabad512910d245f19
AyoubBF <ayoub.benfraj013@gmail.com>
2021-09-26 15:28:23 +0200

    début navbar + fond d'écran

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
 
-
-
+<div>
 <div class="page">
   <NavBar/>
-
-  <b-container class="bv-example-row">
+  
+  <!-- <b-container class="bv-example-row">
   <b-row>
     <b-col class="m-10"><Card/></b-col>
     <b-col><Card/></b-col>
     <b-col><Card/></b-col>
   </b-row>
-</b-container>
+</b-container> -->
 
+</div>
 </div>
 
 </template>
@@ -20,3 +20,8 @@
 <script>
   export default {}
 </script>
+<style>
+  body {
+ background-image: url("https://www.oui.sncf/assets/media/2019-08/istock-fabrice-cabaud.jpg");
+}
+</style>
