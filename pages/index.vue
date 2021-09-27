264f22910271ede499138de0f86df9cff3176936
AyoubBF <ayoub.benfraj013@gmail.com>
2021-09-27 17:14:24 +0200

    footer template

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,33 @@
 <template>
-
-<div>
-<div class="page">
+<body>
+  <div id="full-size">
   <NavBar/>
   
-  <!-- <b-container class="bv-example-row">
+  <b-container class="bv-example-row">
   <b-row>
     <b-col class="m-10"><Card/></b-col>
     <b-col><Card/></b-col>
     <b-col><Card/></b-col>
   </b-row>
-</b-container> -->
-
+  <b-row>
+    <b-col class="m-10"><Card/></b-col>
+    <b-col><Card/></b-col>
+    <b-col><Card/></b-col>
+  </b-row>
+  <b-row>
+    <b-col class="m-10"><Card/></b-col>
+    <b-col><Card/></b-col>
+    <b-col><Card/></b-col>
+  </b-row>
+  <b-row>
+    <b-col class="m-10"><Card/></b-col>
+    <b-col><Card/></b-col>
+    <b-col><Card/></b-col>
+  </b-row>
+  </b-container>
+<Footer/>
 </div>
-</div>
-
+</body>
 </template>
 
 <script>
@@ -42,7 +55,14 @@ import NuxtLogoVue from '../components/NuxtLogo.vue'
 </script>
 <style>
 
-  body {
+html,body{ margin:0; padding:0; height:100%; width:100%; }
+#full-size{
+  height:100%;
+  width:100%;
+  overflow:hidden; /* or overflow:auto; if you want scrollbars */
+}
+
+body {
  background-image: url("https://www.oui.sncf/assets/media/2019-08/istock-fabrice-cabaud.jpg");
  background-position: center center;
  background-repeat: no-repeat;
