b18735ccb5231bdce1450cbb43b18e51bfa5f47d
AyoubBF <ayoub.benfraj013@gmail.com>
2021-10-30 15:45:54 +0200

    card content

---
+++ components/Card2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <b-card
+    title="Paimpol : les professionnels de santé s’organisent"
+    img-src="https://mvistatic.com/photosmvi/2018/07/02/P17704927D3481106G.jpg"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+  <b-card-text>
+      Le projet de constitution d’une Communauté professionnelle territoriale de santé est à l’étude et sur le point d’être finalisé.
+    </b-card-text>
+
+    <b-button href="#" variant="danger">En savoir plus
+    </b-button>
+  </b-card>
+</div>
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<style>
+</style>
